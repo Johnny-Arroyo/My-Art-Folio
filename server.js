d90d5afaeba95968623587efe0c9c459a80f7bdc
John Arroyo II <jtarroyo2@gmail.com>
2023-06-07 13:10:54 -0600

    updated files in public directory

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require("http");
+/*const http = require("http");
 const fs = require("fs");
 const port = 3000;
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("index.html", (error, data) => {
+  fs.readFile("public/index.html", (error, data) => {
     if (error) {
       res.writeHead(404);
       res.write("Error: File Not Found");
@@ -20,5 +20,29 @@ server.listen(port, (error) => {
     console.log("Something went wrong", error);
   } else {
     console.log(`Server is running on port ${port}`);
+  }
+});
+*/
+
+const express = require("express");
+const path = require("path");
+const mime = require("mime");
+
+const app = express();
+const port = 3000;
+
+//Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//Start the server
+app.listen(port, (error) => {
+  if (error) {
+    console.log("Something went wrong, error");
+  } else {
+    console.log(`Server running on port ${port}`);
   }
 });
